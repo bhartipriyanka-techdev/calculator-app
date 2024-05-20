9d46007a23b1a40cb408915d0f4663eb455645dd
bhartipriyanka-techdev <priyankabharti.techdev@gmail.com>
2024-05-20 14:58:58 +0530

    I created a Calculator class and provided one method, add.

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+  //------------------Defining Add Method-----------
+  public int add(int number1, int number2) {
+    int result = number1 + number2;
+    return result;
+  }
+
+  
+}
